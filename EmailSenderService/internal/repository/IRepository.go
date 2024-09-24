1cf035b70d187a43bbc90b87ceb50827cf42d3a7
bernardoArcari <BernardoArcari@gmail.com>
2024-09-24 06:39:47 -0300

    refactor: interface name

--- EmailSenderService/internal/repository/IRepository.go
+++ EmailSenderService/internal/repository/IRepository.go
@@ -2,6 +2,6 @@ package repository
 
 import "github.com/BernardoDenkvitts/EmailSenderService/internal/types"
 
-type IRepository interface {
+type ILogRepository interface {
 	Save(emailLog *types.EmailLog) error
 }

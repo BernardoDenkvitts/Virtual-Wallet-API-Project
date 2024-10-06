04b6949fc4d0d90de16e0ad91f9075e560ef2d99
bernardoArcari <BernardoArcari@gmail.com>
2024-10-06 19:21:45 -0300

    fix: missing error message

--- EmailSenderService/utils/utils.go
+++ EmailSenderService/utils/utils.go
@@ -10,6 +10,6 @@ func LoadEnv(path_folder_to_env string) {
 	path, _ := os.Getwd()
 	err := godotenv.Load(path + path_folder_to_env)
 	if err != nil {
-		panic("Error to load .env")
+		panic("Error to load .env erro -> " + err.Error())
 	}
 }

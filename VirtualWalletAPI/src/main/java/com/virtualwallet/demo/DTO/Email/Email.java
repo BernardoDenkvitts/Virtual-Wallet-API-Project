d78e8f58167b06080e3dd74bccd260c3bf51f2ce
bernardoArcari <BernardoArcari@gmail.com>
2024-10-27 01:25:23 -0300

    feat: email dto

--- VirtualWalletAPI/src/main/java/com/virtualwallet/demo/DTO/Email/Email.java
+++ VirtualWalletAPI/src/main/java/com/virtualwallet/demo/DTO/Email/Email.java
@@ -1,11 +1,7 @@
 package com.virtualwallet.demo.DTO.Email;
 
-public class Email {
-    String userEmail;
-    String emailMessage;
-
-    public Email(String userEmail, String emailMessage) {
-        this.userEmail = userEmail;
-        this.emailMessage = emailMessage;
-    }
-}
+public record Email (
+    String to,
+    String subject,
+    String message
+){}
